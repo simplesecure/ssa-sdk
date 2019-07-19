0b2fd1fd9ac6b3e41177ceea854f3d3957aae50f
polluterofminds <justin@graphitedocs.com>
2019-07-19 09:13:44 -0500

    added standalone user session test

--- test/test.js
+++ test/test.js
@@ -18,6 +18,25 @@ const clientKeyPair = {
 //Stand alone tests
 let testKeychain
 
+describe('User session returned', function() {
+  it('should return a valid user session', async function() {
+      const appPrivKey = '8681e1cdaa96c5caf0c5da4e3a49c587b6b468fce89f71bef0525d28ce5450fc';
+      const hubUrl = 'https://hub.blockstack.org';
+      const scopes = ['store_write'];
+      const appOrigin = 'helloblockstack.com'
+      const userData = {
+          appPrivKey,
+          hubUrl,
+          scopes,
+          appOrigin,
+          id: credObj.id
+      }
+      const userSession = await auth.makeUserSession(userData);
+
+      assert(userSession.message, "user session created");
+  })
+});
+
 describe("NameLookUp", function() {
   this.timeout(7000);
   it("name should be available", async function() {
@@ -63,47 +82,28 @@ describe('StoreEncryptedMnemonic', function() {
 //Account Creation
 describe('CreateAccount', function() {
   this.timeout(10000);
-    it('should return account created message', async function() {
-        const create = await auth.createUserAccount(credObj, appObj);
-        console.log(create)
-        assert.equal(create.message,"successfully created user session")
-    });
+  it('should return account created message', async function() {
+      const create = await auth.createUserAccount(credObj, appObj);
+      console.log(create)
+      assert.equal(create.message,"successfully created user session")
+  });
 });
 
 
 // //Log In
 // describe('LogIn', function() {
-//     describe('user session returned', function() {
-//         it('should return a valid user session', async function() {
-//             const appPrivKey = '8681e1cdaa96c5caf0c5da4e3a49c587b6b468fce89f71bef0525d28ce5450fc';
-//             const hubUrl = 'https://hub.blockstack.org';
-//             const scopes = ['store_write'];
-//             const appOrigin = 'helloblockstack.com'
-//             const userData = {
-//                 appPrivKey,
-//                 hubUrl,
-//                 scopes,
-//                 appOrigin,
-//                 id: creds.id
-//             }
-//             const userSession = await auth.makeUserSession(userData);
+//   this.timeout(10000);
+//   it('should use supplied username and password to log in', async function() {
+//     const credObj = creds;
+//     const appObj = {
+//       hubUrl: 'https://hub.blockstack.org',
+//       scopes: ['store_write'],
+//       appOrigin: 'helloblockstack.com'
+//     }
+//     const loggedIn = await auth.login(credObj, appObj);
 
-//             assert(userSession.message, "user session created");
-//         })
-//     });
-//     describe('full login', function() {
-//       it('should use supplied username and password to log in', async function() {
-//         const credObj = creds;
-//         const appObj = {
-//           hubUrl: 'https://hub.blockstack.org',
-//           scopes: ['store_write'],
-//           appOrigin: 'helloblockstack.com'
-//         }
-//         const loggedIn = await auth.login(credObj, appObj);
-
-//         assert(loggedIn.message, "user session created");
-//       })
-//     })
+//     assert(loggedIn.message, "user session created");
+//   })
 // });
 
 //BlockstackJS Operations
