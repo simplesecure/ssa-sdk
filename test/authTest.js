90f2e456a799e67b6ae7b7b229c661d1f62eea2d
polluterofminds <justin@graphitedocs.com>
2019-08-29 09:22:34 -0500

    updated test file

--- test/authTest.js
+++ test/authTest.js
@@ -58,6 +58,7 @@ module.exports = {
     .then(async (body) => {
       // POST succeeded...
       return {
+        success: true,
         message: "successfully created keychain",
         body: body
       }
@@ -66,6 +67,7 @@ module.exports = {
       // POST failed...
       console.log('ERROR: ', error)
       return {
+        success: false,
         message: "failed to create keychain",
         body: error
       }
@@ -123,7 +125,7 @@ module.exports = {
           console.log("Keychain made")
           idAddress = keychain.body;
           //Now we make the profile
-          let profile = await this.makeProfile(config);
+          let profile = this.makeProfile(config);
 
           const appKeyParams = {
             username: credObj.id,
@@ -140,8 +142,12 @@ module.exports = {
               configObj = JSON.parse(appKeys.body).config || {};
               apiKey = JSON.parse(appKeys.body).apiKey || "";
               wallet = JSON.parse(appKeys.body).walet;
-              textile = JSON.parse(appKeys.body).textile || "";
-              const appUrl = JSON.parse(appKeys.body).blockstack.appUrl || "";
+              if(config.authModules && config.authModules.indexOf('textile') > -1) {
+                textile = JSON.parse(appKeys.body).textile;
+              } else {
+                textile = null;
+              }
+            const appUrl = JSON.parse(appKeys.body).blockstack.appUrl || "";
               profile.apps[config.appOrigin] = appUrl;
               //Let's register the name now
               console.log("Registering name...");
@@ -256,8 +262,11 @@ module.exports = {
           configObj = JSON.parse(appKeys.body).config;
           apiKey = JSON.parse(appKeys.body).apiKey || "";
           wallet = JSON.parse(appKeys.body).walet;
-          textile = JSON.parse(appKeys.body).textile || "";
-         
+          if(params.appObj.authModules && params.appObj.authModules.indexOf('textile') > -1) {
+            textile = JSON.parse(appKeys.body).textile;
+          } else {
+            textile = null;
+          }
           profile.apps[params.appObj.appOrigin] = appUrl;
           //Now, we login
           try {
@@ -405,9 +414,7 @@ module.exports = {
     let profile = {
       '@type': 'Person',
       '@context': 'http://schema.org',
-      'apps': {
-        'https://thisisnew.com' : ''
-      }
+      'apps': {}
     }
     if(appObj.scopes.indexOf("publish_data") > -1) {
       profile.apps[appObj.appOrigin] = ""
