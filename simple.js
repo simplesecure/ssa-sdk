550dea28b8967846e5a47f72f0349da42777abbd
polluterofminds <justin@graphitedocs.com>
2019-11-24 09:01:15 -0500

    web3 provider work

--- simple.js
+++ simple.js
@@ -1,6 +1,18 @@
 import { updateProfile, handleAuth } from './simpleidapi/actions';
 import { nameLookUp, registerSubdomain, makeUserSession } from './blockstack/actions';
 import { UserSession, AppConfig } from 'blockstack';
+import { Query } from './utils/query';
+const ProviderEngine = require('web3-provider-engine')
+const CacheSubprovider = require('web3-provider-engine/subproviders/cache.js')
+const FixtureSubprovider = require('web3-provider-engine/subproviders/fixture.js')
+const FilterSubprovider = require('web3-provider-engine/subproviders/filters.js')
+const SubscriptionsSubprovider = require('web3-provider-engine/subproviders/subscriptions')
+const HookedWalletSubprovider = require('web3-provider-engine/subproviders/hooked-wallet.js')
+const NonceSubprovider = require('web3-provider-engine/subproviders/nonce-tracker.js')
+const RpcSubprovider = require('web3-provider-engine/subproviders/rpc.js')
+const Web3 = require('web3');
+const engine = new ProviderEngine()
+const web3 = new Web3(engine)
 const request = require('request-promise');
 const config = require('./config.json');
 const INFURA_KEY = "b8c67a1f996e4d5493d5ba3ae3abfb03"; //TODO: move this to the server to protect key
@@ -8,11 +20,10 @@ const LAYER2_RPC_SERVER = 'https://testnet2.matic.network';
 const SIMPLEID_USER_SESSION = 'SimpleID-User-Session';
 const BLOCKSTACK_DEFAULT_HUB = "https://hub.blockstack.org";
 const BLOCKSTACK_SESSION = 'blockstack-session';
-const Web3 = require('web3'); //TODO: remove web3 dependency
 const ethers = require('ethers');
 let headers = { 'Content-Type': 'application/json', 'Accept': 'application/json' };
-let web3;
 let tx;
+const version = "0.5.0";
 
 function postToApi(options) {
   return request(options)
@@ -34,6 +45,7 @@ function postToApi(options) {
 export default class SimpleID {
   constructor(params) {
     this.config = params;
+    this._selectedAddress = "";
     this.localRPCServer = params.localRPCServer;
     this.network = params.network;
     this.apiKey = params.apiKey;
@@ -41,10 +53,272 @@ export default class SimpleID {
     this.scopes = params.scopes;
     this.appOrigin = params.appOrigin;
     this.development = params.development ? params.development : false;
-    this.provider = new Web3.providers.HttpProvider(this.network === "local" ? this.localRPCServer : this.network === "layer2" ? LAYER2_RPC_SERVER : `https://${this.network}.infura.io/v3/${INFURA_KEY}`);
-    web3 = new Web3(this.provider);
+    
+    this.provider = this._initProvider();
+    //this.provider = new Web3.providers.HttpProvider(this.network === "local" ? this.localRPCServer : this.network === "layer2" ? LAYER2_RPC_SERVER : `https://${this.network}.infura.io/v3/${INFURA_KEY}`);
+    this.subProvider = new Web3.providers.HttpProvider(this.network === "local" ? this.localRPCServer : this.network === "layer2" ? LAYER2_RPC_SERVER : `https://${this.network}.infura.io/v3/${INFURA_KEY}`);
+    //web3 = new Web3(this.provider);
     headers['Authorization'] = this.apiKey;
     this.simple = ethers;
+  }
+
+  _initProvider() {
+    const engine = new ProviderEngine();
+    const query = new Query(engine);
+
+    engine.send = (payload, callback) => {
+      // Web3 1.0 beta.38 (and above) calls `send` with method and parameters
+      if (typeof payload === 'string') {
+        return new Promise((resolve, reject) => {
+          engine.sendAsync(
+            {
+              jsonrpc: '2.0',
+              id: 42,
+              method: payload,
+              params: callback || [],
+            },
+            (error, response) => {
+              if (error) {
+                reject(error);
+              } else {
+                resolve(response.result);
+              }
+            },
+          );
+        });
+      }
+
+      // Web3 1.0 beta.37 (and below) uses `send` with a callback for async queries
+      if (callback) {
+        engine.sendAsync(payload, callback);
+        return;
+      }
+
+      let result = null;
+      switch (payload.method) {
+        case 'eth_accounts':
+          result = this._selectedAddress ? [this._selectedAddress] : [];
+          break;
+
+        case 'eth_coinbase':
+          result = this._selectedAddress ? [this._selectedAddress] : [];
+          break;
+
+        case 'net_version':
+          result = this._network;
+          break;
+
+        case 'eth_uninstallFilter':
+          engine.sendAsync(payload, _ => _);
+          result = true;
+          break;
+
+        default:
+          var message = `The Portis Web3 object does not support synchronous methods like ${
+            payload.method
+          } without a callback parameter.`;
+          throw new Error(message);
+      }
+
+      return {
+        id: payload.id,
+        jsonrpc: payload.jsonrpc,
+        result: result,
+      };
+    };
+
+    engine.addProvider(
+      new FixtureSubprovider({
+        web3_clientVersion: `Portis/v${this.config.version}/javascript`,
+        net_listening: true,
+        eth_hashrate: '0x00',
+        eth_mining: false,
+        eth_syncing: true,
+      }),
+    );
+
+    engine.addProvider(new CacheSubprovider());
+    engine.addProvider(new SubscriptionsSubprovider());
+    engine.addProvider(new FilterSubprovider());
+    engine.addProvider(new NonceSubprovider());
+    engine.addProvider({
+      setEngine: _ => _,
+      handleRequest: async (payload, next, end) => {
+        if (!payload.id) {
+          payload.id = 42;
+        }
+        next();
+      },
+    });
+
+    engine.addProvider(
+      new HookedWalletSubprovider({
+        getAccounts: async cb => {
+          const address = this.getUserData() && this.getUserData().wallet ? this.getUserData().wallet.ethAddr : "";
+          console.log(address);
+          let addresses = [];
+          addresses.push(address);
+          const result = addresses;
+          let error;
+          if (result) {
+            this._selectedAddress = result[0];
+          } else {
+            error = "Trouble getting account"
+          }
+          cb(error, result);
+        },
+        approveTransaction: async (txParams, cb) => {          
+          console.log("APPROVE TX, ", txParams);
+          const email = this.getUserData().email;
+          const code = document.getElementById('token').value;
+          if(code) {
+            const params = { 
+              email, 
+              code, 
+              contract: false
+            };
+            const signed = await this.broadcastTransaction(params);
+            console.log(signed);            
+            if(signed.success) {
+              console.log("Signed and broadcasted. TX Hash: ", signed.body);
+            } else {
+              console.log("Trouble sending transaction...")
+            }
+          } else {            
+            const email = this.getUserData().email;
+            const params = { 
+              email, 
+              fromEmail: "hello@simpleid.xyz", 
+              txObject: txParams 
+            }
+            const approval = await this.generateApproval(params);
+            console.log(approval);
+            console.log("Waiting for approval...");
+          }
+        }, 
+        signTransaction: async (txParams, cb) => {
+          console.log("SIGN TX, ", txParams);
+          let result;
+          let error;
+          const code = document.getElementById('token').value;
+          if(code) {
+            try {
+              const sign = await this.signTx(email, code);
+              console.log(sign);
+              console.log("sending signed tx...")
+            } catch(e) {
+              error = e;
+            }
+          }
+          cb(error, result);
+        },
+        signMessage: async (msgParams, cb) => {
+          console.log("Message tx ", msgParams);
+          console.log("Decoding: ", web3.utils.toUtf8(msgParams.data))
+          //Signing arbitrary data
+          const email = this.getUserData().email;
+          const code = document.getElementById('token').value;
+          if(code) {
+            const signedMessage = await this.signTx(email, code)
+            console.log(signedMessage);
+          } else {
+            const params = {
+              email, 
+              fromEmail: "hello@simpleid.xyz", 
+              txObject: msgParams
+            }
+            const approval = await this.generateApproval(params);
+            console.log(approval);
+          }
+        },
+        signPersonalMessage: async (msgParams, cb) => {
+          console.log("Message tx ", msgParams);
+          console.log("Decoding: ", web3.utils.toUtf8(msgParams.data))
+          //Signing arbitrary data
+          const email = this.getUserData().email;
+          const code = document.getElementById('token').value;
+          if(code) {
+            let error;
+            let result;
+            try {
+              const signedMessage = await this.signTx(email, code)
+              console.log(signedMessage);
+              result = signedMessage;
+            } catch(e) {
+              error = e;
+            }
+            cb(error, result);
+          } else {
+            const params = {
+              email, 
+              fromEmail: "hello@simpleid.xyz", 
+              txObject: msgParams
+            }
+            const approval = await this.generateApproval(params);
+            console.log(approval);
+          }
+        },
+        signTypedMessage: async (msgParams, cb) => {
+          const widgetCommunication = (await this.widget).communication;
+          const params = Object.assign({}, msgParams, { messageStandard: 'signTypedMessage' });
+          const { error, result } = await widgetCommunication.signMessage(params, this.config);
+          cb(error, result);
+        },
+        signTypedMessageV3: async (msgParams, cb) => {
+          const widgetCommunication = (await this.widget).communication;
+          const params = Object.assign({}, msgParams, { messageStandard: 'signTypedMessageV3' });
+          const { error, result } = await widgetCommunication.signMessage(params, this.config);
+          cb(error, result);
+        },
+        estimateGas: async (txParams, cb) => {
+          const gas = await getTxGas(query, txParams);
+          cb(null, gas);
+        },
+        getGasPrice: async cb => {
+          cb(null, '');
+        },
+      }),
+    );
+
+    engine.addProvider({
+      setEngine: _ => _,
+      handleRequest: async (payload, next, end) => {
+        let result;
+  
+          
+        engine.networkVersion = this.network;
+        result = this.network;
+        end("", result);
+      },
+    });
+
+    engine.enable = () =>
+      new Promise((resolve, reject) => {
+        engine.sendAsync({ method: 'eth_accounts' }, (error, response) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(response.result);
+          }
+        });
+      });
+
+    engine.isConnected = () => {
+      return true;
+    };
+
+    engine.isSimpleID = true;
+
+    engine.on('error', error => {
+      if (error && error.message && error.message.includes('PollingBlockTracker')) {
+        console.warn('If you see this warning constantly, there might be an error with your RPC node.');
+      } else {
+        console.error(error);
+      }
+    });
+
+    engine.start();
+    return engine;
   }
 
   //If a developer wants to use the ethers.js library manually in-app, they can access it with this function
@@ -54,6 +328,7 @@ export default class SimpleID {
 
   getProvider() {
     return this.provider;
+    //return this.subProvider;
   }
 
   //This returns the wallet info for the SimpleID user
@@ -69,6 +344,10 @@ export default class SimpleID {
     const appConfig = new AppConfig(this.scopes);
     const userSession = new UserSession({ appConfig });
     return userSession;
+  }
+
+  checkForApproval(txParams, email) {
+    //
   }
 
   async authenticate(payload, options={}) {
@@ -287,9 +566,10 @@ export default class SimpleID {
   }
 
   async broadcastTransaction(params) {
+    console.log("Broadcasting...")
     const { devId, development, network } = this.config;
-    const { email, code, threeBox, contractTx, threeBoxTx } = params;
-    const provider = new ethers.providers.Web3Provider(this.provider);
+    const { email, code, threeBox, contractTx, threeBoxTx, contract } = params;
+    //const provider = new ethers.providers.Web3Provider(this.provider);
     const payload = {
       devId,
       email, 
@@ -298,6 +578,7 @@ export default class SimpleID {
       code,
       threeBox,
       threeBoxTx, 
+      contract,
       development: development ? true : false
     }
     headers['Authorization'] = this.apiKey;
@@ -313,10 +594,23 @@ export default class SimpleID {
     }
   }
 
-  async signTransaction(params) {
-    //There are times when a message or piece of data needs to be signed but 
-    //The actual data doesn't need to be broadcast on chain.
-    //TODO: build this
+  async signTx(email, code) {
+    const { devId, development, network } = this.config;
+    const payload = JSON.stringify({
+      email,
+      network, 
+      devId, 
+      code, 
+      development
+    });
+    headers['Authorization'] = this.apiKey;
+    var options = { url: config.SIGN_TX, method: 'POST', headers: headers, body: payload };
+    try {
+      const postData = await postToApi(options);
+      return postData.body;
+    } catch(error) {
+      return { success: false, body: error }
+    }
   }
 
   async generateApproval(params) {
